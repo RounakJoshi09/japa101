90426f12bf99decde8469fa2508ba0c5ca3469c8
RounakJoshi09 <rounakj@byteridge.com>
2023-06-03 13:39:38 +0530

    Create User Controller Completed

--- controllers/users.js
+++ controllers/users.js
@@ -7,12 +7,14 @@ const create = async (params) =>{
     const { email, firstName, lastName, photoUrl } = params;
     if (!db.isInitialized)
     await db.initialize();
-    if(await db.User.findOne({ where: { email: params.email } })){ 
-        throw new Error('Email already exists');
+    let user = await db.User.findOne({ where: { email: params.email } })
+    if(user){ 
+       return user;
     }
-    const user = new db.User({email,firstName,lastName,photoUrl});
-
-    await user.save();
+    else{
+     user = new db.User({email,firstName,lastName,photoUrl});
+     await user.save();
+    }
 
     return user;
 }
